1403c57bff09c961dbb4e6910e819fd442637966
SNValadez <snvaladez13@gmail.com>
2021-09-19 18:46:41 -0700

    form handler and variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,12 @@
 //the temperature, the humidity, the wind speed, and the UV index
 
 var apiKey = "ca63269de287a28079e1710279049971";
-
-var city = "Dallas";
+var cityInput = document.querySelector("#city-input");
 var example = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
-var ex2 = `api.openweathermap.org/data/2.5/weather?q=Dallas&appid=${apiKey}`
+// var ex2 = `api.openweathermap.org/data/2.5/weather?q=Dallas&appid=${apiKey}`
+var cityBtn = document.querySelector("#search-btn");
+var cityNameEl = document.querySelector("#city-name");
+var cityArr = [];
 
 console.log(example);
 
@@ -21,4 +23,23 @@ function myFunction() {
 }
 
 myFunction();
+
+var formHandler = function(event) {
+
+  var selectedCity = cityInput
+    .value
+    .toLowerCase()
+    .trim()
+    .split(' ')
+    .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+    .join(' ');
+
+    if (selectedCity) {
+      getCoords(selectedCity);
+      cityInput.value = '';
+    } else {
+      alert("Please enter a city!");
+    };
+};
+
 
